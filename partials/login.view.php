b56dc0f076c0b5317ef7c24066cb181704defddb
Saul Eduardo Cova Carrasco <saul.carrasco@eldorado.org.br>
2021-07-14 19:35:57 -0400

    Login section

--- partials/login.view.php
+++ partials/login.view.php
@@ -1,1 +1,11 @@
-<p>Login</p>
+<form action="">
+  <div class="from-group">
+    <input type="text" name="user" id="user"/>
+  </div>
+  <div class="from-group">
+    <input type="password" name="password" id="password"/>
+  </div>
+  <div class="from-group">
+    <button type="submit">INGRESAR</button>
+  </div>
+</form>
