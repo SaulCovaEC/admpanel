1ed4bab272d68d56558c2186d7f1f0cbc7339366
Saul Eduardo Cova Carrasco <saul.carrasco@eldorado.org.br>
2021-07-18 12:19:48 -0400

    Login section

--- login.php
+++ login.php
@@ -1,8 +1,12 @@
 <?php
 require_once './config.php';
 
-require_once './partials/header.restricted.view.php';
-
-require_once './partials/login.view.php';
-
-require_once './partials/footer.restricted.view.php';
+if(isset($_SESSION['user'])){
+  require_once './partials/header.restricted.view.php';
+  
+  require_once './partials/login.view.php';
+  
+  require_once './partials/footer.restricted.view.php';
+} else {
+  header('Location: '.host().'/login');
+}
