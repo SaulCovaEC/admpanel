968d21fc06a20597c7e4ed1e0eae81274583acbc
Saul Eduardo Cova Carrasco <saul.carrasco@eldorado.org.br>
2021-07-18 19:28:59 -0400

    Login section

--- test.php
+++ test.php
@@ -5,11 +5,4 @@ require_once './functions/session.php';
 
 $user = checkUser('saulcova.ec@gmail.com', 'Sllax20.13');
 
-if($user) {
-  $_SESSION['id_user'] = $user['id'];
-  $_SESSION['name'] = $user['name'];
-  echo $_SESSION["name"];
-} else {
-  echo $user;
-  echo "\nNo hay sesion";
-}
+echo $user.name;
