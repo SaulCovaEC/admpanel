ac6a154d344517a0bd7a7beb7c669b7e546c8608
Saul Eduardo Cova Carrasco <saul.carrasco@eldorado.org.br>
2021-07-18 19:21:53 -0400

    Login section

--- functions/session.php
+++ functions/session.php
@@ -1,18 +1,17 @@
 <?php
-//require_once 'dbcnfg.php';
+require_once 'dbcnfg.php';
 
 function checkUser($mail, $pass) {
     $result = array();
     $userSql = "SELECT id_user, name FROM users WHERE email = '$mail' AND pass = '$pass' LIMIT 1";
 
-    //$userResult = mysqli_query($GLOBALS['conn'], $userSql);
+    $userResult = mysqli_query($GLOBALS['conn'], $userSql);
     
-    //$userReturn = mysqli_fetch_object($userResult);
+    $userReturn = mysqli_fetch_object($userResult);
         
-    //array_push($result, $userReturn);
+    array_push($result, $userReturn);
 
-    //echo json_encode($result[0]);
-    echo $userSql;
+    echo json_encode($result[0]);
 }
 
 
