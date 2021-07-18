a5b8f21d6d5ea2d805ba43ece4d98d76357126c1
Saul Eduardo Cova Carrasco <saul.carrasco@eldorado.org.br>
2021-07-18 12:24:29 -0400

    Login section

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
 require_once './config.php';
+session_start();
 
 if(isset($_SESSION['user'])){
   require_once './partials/header.restricted.view.php';
@@ -8,5 +9,5 @@ if(isset($_SESSION['user'])){
   
   require_once './partials/footer.restricted.view.php';
 } else {
-  header('Location: '.host().'/login');
+  header('Location: https://admpanel.hermanitos.org.br/login');
 }
