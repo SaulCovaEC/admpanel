333c0a9f2dd14cdf7bcb521022132e4ee6914de3
Saul Eduardo Cova Carrasco <saul.carrasco@eldorado.org.br>
2021-07-18 18:20:26 -0400

    Login section

---
+++ session.php
@@ -0,0 +1,27 @@
+<?php
+
+$ch = curl_init();
+
+curl_setopt_array($ch, [
+
+    CURLOPT_URL => 'https://api.hermanitos.org.br/session-login',
+
+    CURLOPT_POST => true,
+
+    CURLOPT_HTTPHEADER => [
+        'Authorization: Bearer ' . $token,
+        'Content-Type: application/json',
+        'x-li-format: json'
+    ],
+
+    CURLOPT_POSTFIELDS => json_encode([
+        'mail' => 'saulcova.ec@gmail.com',
+        'pass' => 'Sllax20.13'
+    ]),
+
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_PROTOCOLS => CURLPROTO_HTTPS
+]);
+
+echo $resultado = curl_exec($ch);
+curl_close($ch);
