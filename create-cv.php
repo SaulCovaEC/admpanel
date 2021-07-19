618aa86fb13bf379c94a33a352dbac5b918c813d
Saul Eduardo Cova Carrasco <saul.carrasco@eldorado.org.br>
2021-07-19 09:39:26 -0400

    Update list cv

---
+++ create-cv.php
@@ -0,0 +1,14 @@
+<?php
+require_once './functions/config.php';
+session_start();
+
+$host = host();
+$login = $host."/login";
+
+if(isset($_SESSION["id_user"])){
+  require_once './partials/header.view.php';
+  require_once './partials/create-cv.view.php';
+  require_once './partials/footer.view.php';
+} else {
+  header("Location: $login");
+}
